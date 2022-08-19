3c726c0d38f163dfe9cc0f39c9c0402212f3cd1d
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-08-19 15:57:14 -0600

    added API tested endpoint

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
-console.log("JS LINKED")
+// console.log("JS LINKED")
+// window.alert("jQuery is linked")
 
-window.alert("jQuery is linked")
+// const $name = $("#name")
+
+const monMoves = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://pokemon-go1.p.rapidapi.com/current_pokemon_moves.json",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "e07529496bmsha2822632d264d48p1f5921jsnffeb73a0f472",
+		"X-RapidAPI-Host": "pokemon-go1.p.rapidapi.com"
+	}
+};
+
+$.ajax(monMoves).done(function (data) {
+	// console.log(data);
+    // console.log(response[0]); // returns bulbasaur pokemon moves
+});
+
+const monCp = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://pokemon-go1.p.rapidapi.com/pokemon_max_cp.json",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "e07529496bmsha2822632d264d48p1f5921jsnffeb73a0f472",
+		"X-RapidAPI-Host": "pokemon-go1.p.rapidapi.com"
+	}
+};
+
+$.ajax(monCp).done(function (data) {
+    console.log(data[0].pokemon_name); //returns idx "0" pokemon name : bulbasaur
+    // const found = data.find((pokeName) => pokeName === "Bulbasaur");
+    // console.log(`found the data!: ${found}`);
+});
