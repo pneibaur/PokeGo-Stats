a189fe68365e5fd7a113ddd6f92c30c295d803f2
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-08-22 14:30:11 -0600

    filtered only Normal pokemon, added types display

--- js/script.js
+++ js/script.js
@@ -43,6 +43,17 @@ const monStats = {
 	}
 };
 
+const monTypes = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://pokemon-go1.p.rapidapi.com/pokemon_types.json",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "e07529496bmsha2822632d264d48p1f5921jsnffeb73a0f472",
+		"X-RapidAPI-Host": "pokemon-go1.p.rapidapi.com"
+	}
+};
+
 
 ////////////////////////////
 //////// VARIABLES
@@ -54,7 +65,8 @@ const $input = $("#inputText")
 
 // main content elements
 const $pokeName = $("#pokeName")
-const $maxCp = $(".maxCp")
+const $pokeType = $("#pokeType")
+const $maxCp = $("#maxCp")
 // base stats elements
 const $attackLi = $("#liAttack")
 const $defenseLi = $("#liDefense")
@@ -88,25 +100,32 @@ function getPokeStats(event){
 	$.ajax(monCp).done(function (data) {
 		// "data" is the entire array of the api data for maxCP pokemon. 
 		// structured as [{k:v, k:v, k:v},{},{}]
-		let idxNum = data.findIndex((e) => e.pokemon_name.toLowerCase() === userInput);
+		let idxNum = data.findIndex((e) => (e.pokemon_name.toLowerCase() === userInput && e.form === "Normal"));
 		$pokeName.text(`Name: ${data[idxNum].pokemon_name}`);
 		$maxCp.text(`Max Combat Power: ${data[idxNum].max_cp}`);
 	});
 
 	// function gets pokemon possible moves
 	$.ajax(monMoves).done(function (data) {
-		let idxNum = data.findIndex((e) => e.pokemon_name.toLowerCase() === userInput);
-		console.log(`charged move idx: ${idxNum}`)
-		console.log(`chargedLi Text: ${$chargedLi.text()}`)
-		$chargedLi.text(`Charged Moves: ${data[idxNum].charged_moves.join(", ")}`)
+		let idxNum = data.findIndex((e) => (e.pokemon_name.toLowerCase() === userInput && e.form === "Normal"));
+		$chargedLi.text(`Charged Moves: ${data[idxNum].charged_moves.join(", ")}`);
+		$fastLi.text(`Fast Moves ${data[idxNum].fast_moves.join(", ")}`);
 	});
 
 	// funciton gets pokemon base stats. 
 	$.ajax(monStats).done(function (data) {
-		let idxNum = data.findIndex((e) => e.pokemon_name.toLowerCase() === userInput);
+		let idxNum = data.findIndex((e) => (e.pokemon_name.toLowerCase() === userInput && e.form === "Normal"));
 		$attackLi.text(`Attack: ${data[idxNum].base_attack}`);
 		$defenseLi.text(`Defense: ${data[idxNum].base_defense}`);
 		$hpLi.text(`Hit Points: ${data[idxNum].base_stamina}`);
+	});
+
+	// function gets pokemon types. 
+	$.ajax(monTypes).done(function (data) {
+		// let filterData = data.filter((e) => e.form === "Normal")
+		let idxNum = data.findIndex((e) => (e.pokemon_name.toLowerCase() === userInput && e.form === "Normal"));
+		console.log(`data item: ${data[idxNum]}`)
+		$pokeType.text(`Type: ${data[idxNum].type.join(", ")}`);
 	});
 	$input.val("");
 }
